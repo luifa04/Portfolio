28f81bec69c498ba3b8a4f7d8e4f4fdf1910bd4e
Mateo Lopez <mateolopez27.11@gmail.com>
2023-10-16 18:58:02 -0300

    first commit

---
+++ src/helpers/navigationLinks.js
@@ -0,0 +1,22 @@
+export const navigationLinks = [
+    {
+        home: 'HOME',
+        ref: '#home'
+    },
+    {
+        home: 'ABOUT ME',
+        ref: '#about-me'
+    },
+    {
+        home: 'TECNOLOGIES',
+        ref: '#tecnologies'
+    },
+    {
+        home: 'PORTFOLIO',
+        ref: '#portfolio'
+    },
+    {
+        home: 'CONTACT',
+        ref: '#constac'
+    }
+]
---
+++ src/screens/navigationBar/NavigationBar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Navigation.css'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import {navigationLinks} from '../../helpers/navigationLinks'
+
+
+function createLink(){
+    return navigationLinks.map((e , idx) =>(
+        <Nav.Link key={idx} href={e.ref}>{e.home} </Nav.Link>
+    ))
+}
+
+function NavigationBar() {
+  return (
+    <div id='home'>
+        <Navbar className='navigation_container' expand='md'>
+        <Navbar.Brand style={{marginLeft:'1rem'}} href="#home">Mateo Lopez Michelini</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse style={{justifyContent: 'flex-end', marginRight:'1rem', borderColor: 'none'}}>
+          <Nav className='links' style={{margin: '0 1rem'}}>
+            {createLink()}
+          </Nav>
+        </Navbar.Collapse>
+        </Navbar>
+    </div>
+  )
+}
+
+export default NavigationBar
